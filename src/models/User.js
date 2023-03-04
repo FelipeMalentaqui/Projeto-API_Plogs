7b80b4aa08e20d29b7b3e7a6792e4408665d983c
Felipe <felipemalentaq@hotmail.com>
2023-03-04 18:42:39 -0300

    requisito  2 ok

---
+++ src/models/User.js
@@ -0,0 +1,27 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+    },
+    displayName:DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  }, {
+    timestamps: false,
+    tableName: 'users',
+    underscored: true,
+  });
+
+  User.associate = (models) => {
+    User.hasMany(models.BlogPost,
+      { foreignKey: 'userId',  // quem que eu tenho que colocar nessa foreignKey? | coluna  referente BlogPost
+      as: 'blog_posts' // quem eu tenho que colocar no as ? | apelido do meu BlogPost
+    });
+  }
+
+  return User;
+};
